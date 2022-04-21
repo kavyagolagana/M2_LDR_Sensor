ca72148fa0a86c087541f63bd6f18de9d84c15a1
Sri Kavya <101033786+kavyagolagana@users.noreply.github.com>
2022-04-21 20:09:02 +0530

    Update function.c

--- 3_Implementation/src/function.c
+++ 3_Implementation/src/function.c
@@ -1,1 +1,42 @@
-
+/**
+ * @file function.
+ * @brief 
+ * @version 0.1
+ * 
+ * 
+ */
+#include "main.h"
+void setup() {
+    for (int i = 2; i < 6; i++)
+      pinMode(i, OUTPUT);
+  
+    Serial.begin(9600);
+  }
+  
+  void loop() 
+  {    
+    for (int i = 2; i < 6; i++)
+      digitalWrite(i, LOW);
+  
+    status_Indicator();  
+  }
+  
+  void status_Indicator()
+  {
+    int sensor_In = analogRead(A0);
+    Serial.println(sensor_In);
+    
+    if ( sensor_In >=400  )
+    {
+      digitalWrite(RED, HIGH);
+      digitalWrite(Buzzer,HIGH);
+    }
+    else if (sensor_In >=160 )
+    {
+      digitalWrite(ORANGE, HIGH);
+    } else
+    {
+      digitalWrite(GREEN, HIGH);
+    }
+    delay(10);
+  }
