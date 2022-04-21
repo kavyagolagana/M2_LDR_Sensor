f5cd63f5ca3768a23c248d75e4890e3dc32ccf53
Sri Kavya <101033786+kavyagolagana@users.noreply.github.com>
2022-04-21 20:05:39 +0530

    Create main.h

---
+++ 3_Implementation/inc/main.h
@@ -0,0 +1,15 @@
+/**
+ * @file main.h
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-21
+ * 
+ * 
+ **/
+
+  # define GREEN 2
+  # define ORANGE 3
+  # define RED 4
+  # define Buzzer 5
+  # define Sensor A0
+    
